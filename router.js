13a0472c36a967338ac942525b2fd12bc653038e
yeon <xxyeon129@gmail.com>
2024-06-16 21:58:32 +0900

    Feat: fashion page routing

--- router.js
+++ router.js
@@ -5,16 +5,23 @@ const ID_CONTENT = 'content-';
 const PATH = {
   HOME: 'home',
   ABOUT_ME: 'aboutme',
+  FASHION_HOME: 'fashion-home',
+  FASHION_TMB: 'fashion-tmb',
+  FASHION_GLOWNY: 'fashion-glowny',
 };
 
 function routing(page) {
   const pages = document.querySelectorAll('.page-content');
+
+  // hide page
   pages.forEach(function (pageContent) {
     pageContent.classList.remove(CLASS_SHOW);
   });
 
+  // show page
   document.getElementById(ID_CONTENT + page).classList.add(CLASS_SHOW);
 
+  // hide header on homepage
   if (page === PATH.HOME) {
     document.querySelector(TAG_HEADER).style.display = 'none';
   } else {
