ee26cabdeec696ac9312cd1a870be3ed7f3102f4
yeon <xxyeon129@gmail.com>
2024-08-04 23:29:04 +0900

    Chore: nextjs initial setting

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <h1>Web Programming Term Project Refactoring</h1>;
+}
--- pages/fashion/common/goToTop.js
+++
@@ -1,7 +0,0 @@
-function goToTop() {
-  window.scroll({
-    top: 0,
-    left: 0,
-    behavior: 'smooth',
-  });
-}
